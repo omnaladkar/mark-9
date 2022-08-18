9cfcfda718f6b658e7c1a6f4930b89203388d15c
omnaladkar <om.naladkar@gmail.com>
2022-08-18 12:09:00 +0530

    first 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import { useState } from "react";
 
-function App() {
+const indexDB = {
+    Indian : [
+    { name: "Nifty", companies: "30" },
+    { name: "Sensex", companies: "50" }
+  ],
+
+  Asian: [
+    {
+      name: "NIKKEI",
+      companies: "225"
+    },
+    {
+      name: "SGX NIFTy",
+      companies: "50"
+    }
+  ],
+  world: [
+    {
+      name: "DAX",
+      companies: "40"
+    },
+    {
+      name: "NASDAQ",
+    companies: "79"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("world");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>  📉 Stock Indexes </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(indexDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {indexDB[selectedGenre].map((stock) => (
+            <li
+              key={stock.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {stock.name} </div>
+              <div style={{ fontSize: "smaller" }}> {stock.companies} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
